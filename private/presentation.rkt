cbfd4e7bcab2ce6944f8e15dec81a77c888c282d
David Christiansen <david@davidchristiansen.dk>
2016-06-14 17:43:06 -0400

    Presentation types for text

--- private/presentation.rkt
+++ private/presentation.rkt
@@ -2,8 +2,15 @@
 
 (require racket/gui/base)
 (require racket/class racket/contract racket/match racket/set)
+(require (for-syntax racket/base syntax/parse))
 
-(provide presentation<%> simple-presentation% presenter<%>
+(provide presentation<%> presenter<%>
+         presentation-type? make-presentation-type presentation-type/c
+         presentation-has-type?
+         presentation-presentation-type presentation-value
+         prop:presentation presentation?
+         (struct-out simple-presentation)
+         presented-object-equal?
          presentation-context<%>
          (contract-out [current-presentation-context
                         (parameter/c (is-a?/c presentation-context<%>))]))
@@ -13,11 +20,38 @@
 
 ;;; A presentation type is an opaque object whose equality is eq?. But
 ;;; the name is saved for debugging purposes.
-(struct presentation-type (name [parent #:auto])
-  #:auto-value #f
+(struct presentation-type (name per make-set)
   #:methods gen:custom-write
   [(define (write-proc x port mode)
      (fprintf port "#<presentation-type:~s>" (presentation-type-name x)))])
+
+(define/contract (make-presentation-type name #:equiv? [per #f] #:empty-set [make-set #f])
+  (->* (symbol?)
+       (#:equiv? (or/c #f (-> any/c any/c any/c))
+        #:empty-set (or/c #f (-> generic-set?)))
+       presentation-type?)
+  (presentation-type name
+                     (or per eq?)
+                     (if per
+                         (or make-set
+                             (slow-set per null))
+                         seteq)))
+
+(define (presented-object-equal? type v1 v2)
+  (define per (presentation-type-per type))
+  (per v1 v2))
+
+(define (value-acceptable? v pres)
+  (define per (presentation-type-per pres))
+  (per v v))
+
+(define (presentation-has-type? presentation type)
+  (eq? (presentation-presentation-type presentation) type))
+
+(define (presentation-type/c pres-type)
+  (make-flat-contract
+   #:name `(presentation-type/c ,pres-type)
+   #:first-order (lambda (x) (value-acceptable? x pres-type))))
 
 ;;; To define a presentation type, one must provide a PER over values
 ;;; and a set datastructure that uses the PER. If no set datastructure
@@ -27,6 +61,8 @@
   [(define (set-member? st v)
      (member v (slow-set-elems st) (slow-set-same? st)))
    (define (set-add st v)
+     (unless ((slow-set-same? st) v v)
+       (raise-argument-error 'set-add "value that is self-equal" v))
      (if (member v (slow-set-elems st) (slow-set-same? st))
          st
          (slow-set (slow-set-same? st) (cons v (slow-set-elems st)))))
@@ -51,42 +87,144 @@
   (define two (set-add one "no"))
   (check-equal? (set->list two) '("oh")))
 
-(define presentation<%>
-  (interface ()
-    [get-presented-object (->m any/c)]
-    [get-modality (->m any/c)]))
+;;; prop:presentation should be set to a two-element list in which the
+;;; first element is the projection to get the presented object and
+;;; the second is the projection to get the presentation type.
+(define-values (prop:presentation presentation? presentation-accessor)
+  (make-struct-type-property
+   'prop:presentation
+   (lambda (x info)
+     (if (and (list? x)
+              (= (length x) 2)
+              (procedure-arity-includes? (car x) 1)
+              (procedure-arity-includes? (cadr x) 1))
+         x
+         (raise-argument-error 'prop:presentation
+                               "A list of two accessors"
+                               x)))))
 
-(define simple-presentation%
-  (class* object% (presentation<%>)
-    (init-field object modality)
-    (super-new)
-    (define/public (get-presented-object) object)
-    (define/public (get-modality) modality)))
+(define/contract (presentation-value pres)
+  (-> presentation? any/c)
+  ((car (presentation-accessor pres)) pres))
+
+(define/contract (presentation-presentation-type pres)
+  (-> presentation? presentation-type?)
+  ((cadr (presentation-accessor pres)) pres))
+
+(struct simple-presentation (value type)
+  #:property prop:presentation
+  (list (lambda (x) (simple-presentation-value x))
+        (lambda (x) (simple-presentation-type x))))
+
+(define not-supplied
+  ((lambda ()
+     (struct not-supplied ()
+       #:methods gen:custom-write
+       [(define (write-proc x port mode)
+          (fprintf port "#<not-supplied>"))])
+     (not-supplied))))
+
+(struct command-argument (name type) #:transparent)
+(struct command (name arguments implementation)
+  #:transparent
+  #:property prop:procedure (struct-field-index implementation)
+  #:methods gen:custom-write
+  [(define (write-proc cmd port mode)
+     (fprintf port "#<command:~a>" (command-name cmd)))])
+
+(begin-for-syntax
+  (define-syntax-class command-argument
+    (pattern (name:id type:expr))))
+
+(define-syntax (define-command stx)
+  (syntax-parse stx
+    [(_ (cmd:id arguments:command-argument ...)
+        body:expr ...)
+     #'(define cmd
+         (command 'cmd
+                  (list (command-argument 'arguments.name arguments.type) ...)
+                  (contract
+                   (-> (presentation-type/c arguments.type)
+                       ...
+                       any/c)
+                   (lambda (arguments.name ...)
+                     body ...)
+                   'command
+                   'other
+                   'cmd
+                   #f)))]
+    [(_ cmd:id body:expr)
+     #'(define-command (cmd) body:expr)]))
+
+(define-syntax (command-lambda stx)
+  (syntax-parse stx
+    [(_ (arguments:command-argument ...)
+        body:expr ...)
+     (with-syntax ([name (gensym 'anonymous-command)])
+       #'(command 'name
+                  (list (command-argument 'arguments.name arguments.type) ...)
+                  (contract
+                   (-> (presentation-type/c arguments.type)
+                       ...
+                       any/c)
+                   (lambda (arguments.name ...)
+                     body ...)
+                   'command
+                   'other
+                   'name
+                   #f)))]))
+
+(define command/p
+  (make-presentation-type
+   'command/p
+   #:equiv? (lambda (x y)
+              (and (command? x)
+                   (command? y)
+                   (eq? x y)))
+   #:empty-set seteq))
+
+(define presentation<%>
+  (interface*
+   ()
+   ([prop:presentation (list (lambda (x) (send x get-presented-value))
+                             (lambda (x) (send x get-presentation-type)))])
+   [get-presentation-type
+    (->m presentation-type?)]
+   [get-presented-value
+    (->i ([me any/c])
+         (result (me) (presentation-type/c (send me get-presentation-type))))]))
 
 (define presenter<%>
   (interface ()
-    ;; Presentations of this object should be shown in an active state
-    [activate (->m any/c void?)]
-    ;; Deactivae all
-    [deactivate (->m void?)]
-    ;; Listen for state changes
-    [new-context-state (->m any/c void?)]))
+    ;; Equivalent presentations should be shown in a highlighted state.
+    [highlight (->i ([me any/c]
+                     [p-t presentation-type?]
+                     [value (p-t) (presentation-type/c p-t)])
+                    ()
+                    [result void?])]
+    ;; No more highlighting.
+    [no-highlighting (->m void?)]))
 
 (define presentation-context<%>
   (interface ()
-    [get-state (->m (or/c #f
-                          (list/c 'accepting (-> any/c any/c any/c) (-> any/c void?))))]
-    [accepting? (->m boolean?)]
-    [accept (->m (-> any/c any/c any/c) (-> any/c void?) void?)]
-    [make-active (->m any/c void?)]
-    [nothing-active (->m void?)]
-    [accepted (->m any/c void?)]
-    [register-command-translator (->m (-> any/c
-                                          any/c
-                                          (listof (list/c string? (-> void?))))
-                                      void?)]
-    [commands-for (->m (is-a?/c presentation<%>)
-                       (listof (list/c string? (-> void?))))]))
+    [register-presenter (->m (is-a?/c presenter<%>) void?)]
+
+    [currently-accepting (->m (or/c #f presentation-type?))]
+    [accept (->i ([me any/c]
+                  [p-t presentation-type?]
+                  [callback (p-t) (-> (presentation-type/c p-t) any/c)])
+                 ()
+                 [result void?])]
+    [accepted (->i ([me any/c]
+                    [pres (me) (let ([accepting-now (send me currently-accepting)])
+                                 (and/c presentation?
+                                        (lambda (pres)
+                                          (eq? (presentation-presentation-type pres)
+                                               accepting-now))))])
+                   ()
+                   [result any/c])]
+    [make-active (->m presentation? void?)]
+    [nothing-active (->m void?)]))
 
 ;;; A presentation context manages the global application presentation
 ;;; state, including:
@@ -98,46 +236,39 @@
 (define presentation-context%
   (class* object% (presentation-context<%>)
     (super-new)
-    ;; The state can be:
-    ;; - #f, the default state
-    ;; - `(accepting ,pred? ,action)
-    (define state #f)
+    (define accepting-stack null)
+    (define/public (currently-accepting)
+      (if (pair? accepting-stack)
+          (caar accepting-stack)
+          #f))
 
-    (define/public (get-state)
-      state)
-
-    (define (set-state new-state)
-      (set! state new-state)
+    (define (propagate-state new-state)
       (for ([p (in-set presenters)])
-        (send p new-context-state new-state)))
+        (send p accepting (currently-accepting))))
 
-    (define/public (accepting?)
-      (match state
-        [(list-rest 'accepting _) #t]
-        [_ #f]))
+    (define/public (accept type callback)
+      (set! accepting-stack (cons (cons type callback) accepting-stack)))
 
-    (define/public (accept pred action)
-      (set-state `(accepting ,pred ,action)))
+    (define/public (accepted pres)
+      (match-define (cons (cons pres-type callback) todo) accepting-stack)
+      (queue-callback (lambda () (callback (presentation-value pres))))
+      (set! accepting-stack todo))
 
     (define presenters (weak-seteq))
     (define/public (register-presenter presenter)
       (set-add! presenters presenter))
 
-    ;; Instruct all registered presenters to show obj as active
-    (define/public (make-active obj)
-      (for ([p (in-set presenters)])
-        (send p activate obj)))
+    ;; Instruct all registered presenters to show equivalent
+    ;; presentations as active
+    (define/public (make-active p)
+      (for ([presenter (in-set presenters)])
+        (send presenter highlight
+              (presentation-presentation-type p)
+              (presentation-value p))))
 
     (define/public (nothing-active)
       (for ([p (in-set presenters)])
-        (send p deactivate)))
-
-    (define/public (accepted obj)
-      (match state
-        [(list 'accepting pred act)
-         (act obj)
-         (set-state #f)]
-        [_ (raise-argument-error 'accepted "Not accepting" obj)]))
+        (send p no-highlighting)))
 
     ;; Presentations can be implictly considered as commands, which
     ;; will be presented in a menu.  A presentation-to-command
@@ -155,3 +286,6 @@
 
 (define current-presentation-context
   (make-parameter (new presentation-context%)))
+
+(define (accept presentation-type callback #:presentation-context [context (current-presentation-context)])
+  (send context accept presentation-type callback))
